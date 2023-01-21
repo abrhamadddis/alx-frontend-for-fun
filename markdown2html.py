f08593471b8fd41b66932fb4a70dc91fcdb0fdc9
abrhamadddis <abrhamaddis32@gmail.com>
2023-01-21 22:26:26 +0300

    start script

---
+++ markdown2html.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" check the the requirment"""
+from os import path
+import sys
+if __name__ == "__main__":
+    if(len(sys.argv) < 2):
+        sys.stderr.write("Usage: ./markdown2html.py README.md README.html")
+        sys.exit(1)
+    elif (not path.exists(sys.argv[1])):
+        sys.stderr.write("Missing {}".format(sys.argv[1]))
+        sys.exit(1)
+    else:
+        sys.exit(0)

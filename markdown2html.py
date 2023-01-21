f398be10d565ed6c34a9b94dcb155fc9c7a4929e
abrhamadddis <abrhamaddis32@gmail.com>
2023-01-21 22:33:20 +0300

    fixed

--- markdown2html.py
+++ markdown2html.py
@@ -6,8 +6,8 @@ if __name__ == "__main__":
     if(len(sys.argv) < 3):
         sys.stderr.write("Usage: ./markdown2html.py README.md README.html")
         sys.exit(1)
-    elif (not path.exists(sys.argv[0])):
-        sys.stderr.write("Missing {}".format(sys.argv[0]))
+    elif (not path.exists(sys.argv[1])):
+        sys.stderr.write("Missing {}".format(sys.argv[1]))
         sys.exit(1)
     else:
         sys.exit(0)
